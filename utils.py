2dc8b6371d105889ac721428b55444ff5acc3ad9
luckysher <luckyshera95@gmail.com>
2017-04-03 23:05:50 +0530

    updated util file

--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ spreadsheetId = 'mygooglespreadsheetid'
 # credentials file name
 CREDENTIALS_FILE = 'credential.json'
 
+# data sample file path
+XL_FILE_PATH = '/path/to/data/Samples/data.xlsx'
+
 # set settings for the logger and return logger
 def getLogger():
     logging.basicConfig(format='[%(name)s][%(levelname)s] %(message)s', level=logging.DEBUG)

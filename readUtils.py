71a04e27b3ebfe9408db7741e68b5ae76736b9bb
luckysher <luckyshera95@gmail.com>
2017-04-03 23:34:36 +0530

    added fun for reading excel file data

--- readUtils.py
+++ readUtils.py
@@ -11,4 +11,23 @@ class XLRead:
             print("[%s] readUtils got exception while reading file.." % APP_NAME)
             print("[%s] %s" %( APP_NAME, e))
 
-    
+    def readData(self, startRow, endRow=None):
+        cursheet = self.xlbook.sheet_by_index(0)
+        endRow = cursheet.nrows
+        print("Sr:   Name     Age   Marks")
+        for row in range(startRow, endRow):
+            # get data in the current row
+            rowDataList = cursheet.row(row)
+
+            # check for empty row
+            if rowDataList[0].value == '' and rowDataList[1].value == '' and rowDataList[2].value == '' and rowDataList[3].value == '':
+                continue    # no need to process the row further
+            try:
+                id = int(rowDataList[0].value)
+                name = str(rowDataList[1].value)
+                age = int(rowDataList[2].value)
+                marks = int(rowDataList[3].value)
+
+                print("%d   %s     %d   %d" % (id, name, age, marks))
+            except Exception as e:
+                pass

2b634387aaa3838b6709ad8dec747d2aec79e2cc
luckysher <luckyshera95@gmail.com>
2017-04-03 22:12:05 +0530

    updated utils for and added logger and other settings

--- utils.py
+++ utils.py
@@ -2,4 +2,17 @@ import logging
 
 APP_NAME = 'EXCEL TO GOOGLE SHEET'
 
+CLIENT_SECRET_FILE = 'client_secret.json'
+
+# google sheet Id from which to read
+spreadsheetId = 'mygooglespreadsheetid'
+
+# credentials file name
+CREDENTIALS_FILE = 'credential.json'
+
+# set settings for the logger and return logger
+def getLogger():
+    logging.basicConfig(format='[%(name)s][%(levelname)s] %(message)s', level=logging.DEBUG)
+    logger = logging.getLogger(APP_NAME)
+    return logger
 

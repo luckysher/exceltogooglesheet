4b1a9c86f4c70c439aeef67505d4ece9fcdfec68
luckysher <luckyshera95@gmail.com>
2017-04-05 16:23:50 +0530

    added main file for running program

---
+++ main.py
@@ -0,0 +1,16 @@
+#####################################################
+# Main file for reading/writing data from the       #
+# excel file to Google datasheet                    #
+#####################################################
+
+from readUtils import *
+from googleUtils import GoogleUtils
+logger = getLogger()
+
+# main fun for calling all the util funs
+def main():
+    pass
+
+if __name__ == "__main__":
+    logger.debug("Starting main app.....")
+    main()
